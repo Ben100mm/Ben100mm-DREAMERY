57dc650b3d8195b9b7ac91a339bcc7eb8ed67514
Benjamin Vincent <benjamin@Mac.lan>
2025-08-05 18:36:03 -0700

    fix: resolve TypeScript errors in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -94,7 +94,7 @@ interface SearchHistory {
   type: SearchResult['type'];
 }
 
-const StyledAutocomplete = styled(Autocomplete<SearchResult | string, boolean, true, false>)`
+const StyledAutocomplete = styled(Autocomplete<SearchResult | string>)`
   flex-grow: 1;
   .MuiInputBase-root {
     color: #1a365d;
